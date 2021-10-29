e43787cc4eaf981e5045f6e1287c5b258b1e41fe
Dave Roye <dave.roye@microgendx.com>
2021-10-29 15:11:46 -0500

    Completed module

---
+++ api/src/router.js
@@ -0,0 +1,37 @@
+const {Router, request, response} = require("express");
+const multer = require("multer");
+
+const storage = multer.diskStorage(
+    {
+        destination: 'api/uploads/',
+        filename: filename
+    });
+const router = Router();
+const upload = multer(
+    {
+        fileFilter: fileFilter,
+        storage: storage
+    });
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+    if (request.fileValidationError) {
+        response.status(400).json({error: request.fileValidationError});
+    } else {
+        response.status(201).json({success: true});
+    }
+});
+    
+function filename(request, file, callback) {
+    callback(null, file.originalname);
+}
+
+function fileFilter(request, file, callback) {
+    if (file.mimetype != 'image/png') {
+        request.fileValidationError = "Wrong file type";
+        callback(null, false, new Error(message="Wrong file type"));
+    } else {
+        callback(null, true);
+    }
+}
+
+module.exports = router;
